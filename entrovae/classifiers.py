fixing bugs and adding heteroscedastic Gaussian NN loglikelihood

entrovae/classifiers.py
@@ -16,7 +16,7 @@ class MNISTClassificationNet(nn.Module):
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
 
-        self.nll = F.nll_loss
+        self.nloglikelihood = F.nll_loss
 
     def forward(self, x):
         # Convolutional layers.
@@ -41,4 +41,4 @@ class MNISTClassificationNet(nn.Module):
     def nll(self, x, y):
         output = self.forward(x)
 
-        return self.nll(output, y), output
+        return self.nloglikelihood(output, y), output

entrovae/loglikelihoods.py
@@ -6,7 +6,7 @@ import numpy as np
 from torch.distributions.normal import Normal
 from .networks import LinearNN
 
-__all__ = ['NNGaussian', 'NNBernoulli']
+__all__ = ['NNHomoGaussian', 'NNHeteroGaussian', 'NNBernoulli']
 
 
 class HomoGaussian(nn.Module):
@@ -25,9 +25,9 @@ class HomoGaussian(nn.Module):
 
     def forward(self, mu, x):
         sigma = self.log_sigma.exp().clamp(min=self.min_sigma)
-        px_mu = Normal(mu, sigma)
+        px = Normal(mu, sigma)
 
-        return px_mu.log_prob(x)
+        return px.log_prob(x)
 
     def predict(self, mu):
         sigma = self.log_sigma.exp().clamp(min=self.min_sigma)
@@ -50,9 +50,9 @@ class Bernoulli(nn.Module):
         return torch.sigmoid(f)
 
 
-class NNGaussian(nn.Module):
+class NNHomoGaussian(nn.Module):
     """A fully connected neural network for parameterising a diagonal
-    Gaussian distribution.
+    Gaussian distribution with homoscedastic noise.
     :param in_dim (int): dimension of the input variable.
     :param out_dim (int): dimension of the output variable.
     :param hidden_dims (list, optional): dimensions of hidden layers.
@@ -80,6 +80,46 @@ class NNGaussian(nn.Module):
         mu = self.network(z)
 
         return self.loglikelhood.predict(mu)
+
+
+class NNHeteroGaussian(nn.Module):
+    """A fully connected neural network for parameterising a diagonal
+    Gaussian distribution with heteroscedastic noise.
+    :param in_dim (int): dimension of the input variable.
+    :param out_dim (int): dimension of the output variable.
+    :param hidden_dims (list, optional): dimensions of hidden layers.
+    :param sigma (float, optional): if not None, sets the initial
+    homoscedastic output sigma.
+    :param sigma_grad (bool, optional): whether to train the homoscedastic
+    output sigma.
+    :param min_sigma (float, optional): sets the minimum output sigma.
+    :param nonlinearity (function, optional): non-linearity to apply in
+    between layers.
+    """
+    def __init__(self, in_dim, out_dim, hidden_dims=(64, 64), min_sigma=1e-3,
+                 nonlinearity=F.relu):
+        super().__init__()
+
+        self.out_dim = out_dim
+        self.min_sigma = min_sigma
+        self.network = LinearNN(in_dim, 2 * out_dim, hidden_dims, nonlinearity)
+
+    def forward(self, z, x):
+        output = self.network(z)
+        mu = output[..., :self.out_dim]
+        raw_sigma = output[..., self.out_dim:]
+        sigma = F.softplus(raw_sigma) + self.min_sigma
+        px = Normal(mu, sigma)
+
+        return px.log_prob(x)
+
+    def predict(self, z):
+        output = self.network(z)
+        mu = output[..., :self.out_dim]
+        raw_sigma = output[..., self.out_dim:]
+        sigma = F.softplus(raw_sigma) + self.min_sigma
+
+        return mu, sigma
 
 
 class NNBernoulli(nn.Module):

entrovae/models.py
@@ -8,13 +8,12 @@ __all__ = ['VAE']
 
 
 class VAE(nn.Module):
-    def __init__(self, encoder, loglikelihood, z_dim, min_sigma=1e-3):
+    def __init__(self, encoder, loglikelihood, z_dim):
         super().__init__()
 
         self.encoder = encoder
         self.loglikelihood = loglikelihood
         self.z_dim = z_dim
-        self.min_sigma = min_sigma
 
     def pz(self, x):
         pz_mu = torch.zeros(x.shape[0], self.z_dim)
@@ -24,7 +23,6 @@ class VAE(nn.Module):
 
     def qz(self, x):
         qz_mu, qz_sigma = self.encoder(x)
-        qz_sigma += self.min_sigma
 
         return qz_mu, qz_sigma
 
@@ -32,6 +30,44 @@ class VAE(nn.Module):
         """Monte Carlo estimate of the evidence lower bound."""
         pz_mu, pz_sigma = self.pz(x)
         qz_mu, qz_sigma = self.qz(x)
+
+        kl = gaussian_diagonal_kl(qz_mu, qz_sigma, pz_mu, pz_sigma).sum()
+
+        # z_samples is shape (num_samples, batch, z_dim).
+        z_samples = qz_mu + qz_sigma * torch.randn(num_samples, *qz_mu.shape)
+
+        log_px_z = 0
+        for z in z_samples:
+            log_px_z += self.loglikelihood(z, x).sum()
+
+        log_px_z /= num_samples
+        elbo = (log_px_z - kl) / x.shape[0]
+
+        return elbo
+
+
+class EntroVAE(VAE):
+    def __init__(self, encoder, loglikelihood, latent_dim, init_scale=1.):
+        super().__init__(encoder, loglikelihood, latent_dim)
+
+        self.log_scale = nn.Parameter(torch.ones(latent_dim) *
+                                      np.log(init_scale))
+
+    def qz(self, x, h):
+        qz_mu = self.encoder(x)[0]
+
+        qz_sigma = h.unsqueeze(1).matmul(self.log_scale.exp().unsqueeze(0))
+
+        # Reshape.
+        qz_mu = qz_mu.transpose(0, 1)
+        qz_sigma = qz_sigma.transpose(0, 1)
+
+        return qz_mu, qz_sigma
+
+    def elbo(self, x, h, num_samples=1):
+        """Monte Carlo estimate of the evidence lower bound."""
+        pz_mu, pz_sigma = self.pz(x)
+        qz_mu, qz_sigma = self.qz(x, h)
 
         kl = gaussian_diagonal_kl(qz_mu, qz_sigma, pz_mu, pz_sigma)
 
@@ -46,26 +82,5 @@ class VAE(nn.Module):
         elbo = (log_px_z - kl) / x.shape[0]
 
         return elbo
-
-
-class EntroVAE(VAE):
-    def __init__(self, encoder, loglikelihood, latent_dim, init_scale=1.,
-                 min_sigma=1e-3):
-        super().__init__(encoder, loglikelihood, latent_dim, min_sigma)
-
-        self.log_scale = nn.Parameter(torch.ones(latent_dim) *
-                                      np.log(init_scale))
-
-    def qz(self, x, h):
-        qz_mu = self.encoder(x)[0]
-
-        qz_sigma = h.unsqueeze(1).matmul(self.log_scale.exp().unsqueeze(0))
-        qz_sigma += self.min_sigma
-
-        # Reshape.
-        qz_mu = qz_mu.transpose(0, 1)
-        qz_sigma = qz_sigma.transpose(0, 1)
-
-        return qz_mu, qz_sigma
 
 

entrovae/networks.py
@@ -28,7 +28,7 @@ class LinearNN(nn.Module):
                 self.layers.append(nn.Linear(hidden_dims[i-1], hidden_dims[i]))
 
         # Weight initialisation.
-        for layer in self.layers():
+        for layer in self.layers:
             nn.init.xavier_normal_(layer.weight)
             if layer.bias.data is not None:
                 nn.init.constant_(layer.bias, 0)