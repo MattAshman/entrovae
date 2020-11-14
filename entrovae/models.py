7a8b44273995285226e65d8b1d25f7b73fac62ce
MattAshman <matthew.ashman97@gmail.com>
2020-11-14 13:09:44 +0000

    updating gmmvae

--- entrovae/models.py
+++ entrovae/models.py
@@ -2,9 +2,11 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-from .utils import gaussian_diagonal_kl, gaussian_diagonal_ll
+from torch.distributions.normal import Normal
+from torch.distributions.categorical import Categorical
+from torch.distributions.kl import kl_divergence
 
-__all__ = ['VAE', 'EntroVAE']
+__all__ = ['VAE', 'EntroVAE', 'GMMVAE']
 
 
 class VAE(nn.Module):
@@ -18,23 +20,26 @@ class VAE(nn.Module):
     def pz(self, x):
         pz_mu = torch.zeros(x.shape[0], self.z_dim)
         pz_sigma = torch.ones(x.shape[0], self.z_dim)
+        pz = Normal(pz_mu, pz_sigma)
 
-        return pz_mu, pz_sigma
+        return pz
 
     def qz(self, x):
         qz_mu, qz_sigma = self.encoder(x)
+        qz = Normal(qz_mu, qz_sigma)
 
-        return qz_mu, qz_sigma
+        return qz
 
     def elbo(self, x, num_samples=1):
         """Monte Carlo estimate of the evidence lower bound."""
-        pz_mu, pz_sigma = self.pz(x)
-        qz_mu, qz_sigma = self.qz(x)
+        pz = self.pz(x)
+        qz = self.qz(x)
 
-        kl = gaussian_diagonal_kl(qz_mu, qz_sigma, pz_mu, pz_sigma).sum()
+        kl = kl_divergence(qz, pz).sum()
 
         # z_samples is shape (num_samples, batch, z_dim).
-        z_samples = qz_mu + qz_sigma * torch.randn(num_samples, *qz_mu.shape)
+        z_samples = qz.rsample((num_samples,))
+        # z_samples = qz_mu + qz_sigma * torch.randn(num_samples, *qz_mu.shape)
 
         log_px_z = 0
         for z in z_samples:
@@ -44,6 +49,23 @@ class VAE(nn.Module):
         elbo = (log_px_z - kl) / x.shape[0]
 
         return elbo
+
+    def sample(self, num_samples=1):
+        z = torch.randn((num_samples, self.z_dim))
+        samples = self.loglikelihood.predict(z)
+
+        return samples
+
+    def predict_x(self, z):
+        x = self.loglikelihood.predict(z)
+
+        return x
+
+    def reconstruct_x(self, x):
+        z, _ = self.encoder(x)
+        x_recon = self.loglikelihood.predict(z)
+
+        return x_recon
 
 
 class EntroVAE(VAE):
@@ -55,18 +77,20 @@ class EntroVAE(VAE):
     def qz(self, x, h):
         qz_mu = self.encoder(x)[0]
         qz_sigma = h.unsqueeze(1).matmul(self.logscale.exp().unsqueeze(0))
+        qz = Normal(qz_mu, qz_sigma)
 
-        return qz_mu, qz_sigma
+        return qz
 
     def elbo(self, x, h, num_samples=1):
         """Monte Carlo estimate of the evidence lower bound."""
-        pz_mu, pz_sigma = self.pz(x)
-        qz_mu, qz_sigma = self.qz(x, h)
+        pz = self.pz(x)
+        qz = self.qz(x, h)
 
-        kl = gaussian_diagonal_kl(qz_mu, qz_sigma, pz_mu, pz_sigma).sum()
+        kl = kl_divergence(qz, pz).sum()
 
         # z_samples is shape (num_samples, batch, z_dim).
-        z_samples = qz_mu + qz_sigma * torch.randn(num_samples, *qz_mu.shape)
+        z_samples = qz.rsample((num_samples,))
+        # z_samples = qz_mu + qz_sigma * torch.randn(num_samples, *qz_mu.shape)
 
         log_px_z = 0
         for z in z_samples:
@@ -86,6 +110,7 @@ class GMMVAE(nn.Module):
         self.encoder = encoder
         self.loglikelihood = loglikelihood
         self.z_dim = z_dim
+        self.k = k
 
         # Initialise GMM parameters.
         self.pz_y_mu = nn.Parameter(torch.zeros((k, z_dim)),
@@ -95,48 +120,88 @@ class GMMVAE(nn.Module):
 
     def qz(self, x):
         qz_mu, qz_sigma = self.encoder(x)
+        qz = Normal(qz_mu, qz_sigma)
 
-        return qz_mu, qz_sigma
+        return qz
 
     def py_z(self, z, pi):
         # Compute the marginal likelihood, p(z) = \sum_k p(z|y)p(y).
         pzy = torch.zeros_like(pi)
-        for k in range(pi.shape[-1]):
-            pzy[k] = gaussian_diagonal_ll(z, self.pz_y_mu[k, :],
-                                          self.pz_y_logsigma[k, :].exp() ** 2)
+        for k in range(self.k):
+            pz_y = Normal(self.pz_y_mu[k, :], self.pz_y_logsigma[k, :].exp())
+            # pzy[k] = gaussian_diagonal_ll(z, self.pz_y_mu[k, :],
+            #                               self.pz_y_logsigma[k, :].exp() ** 2)
+            pzy[k] = pz_y.log_prob(z)
             pzy[k] += pi[:, k].log()
 
         pz = torch.logsumexp(pzy, dim=1)
 
         # Compute the posterior p(y|z) = p(z, y) / p(z)
         py_z = pzy.exp() / pz
+        py_z = Categorical(py_z)
 
         return py_z
 
     def elbo(self, x, pi, num_samples=1):
         """Monte Carlo estimate of the evidence lower bound."""
-        qz_mu, qz_sigma = self.qz(x)
+        qz = self.qz(x)
 
         # z_samples is shape (num_samples, batch, z_dim).
-        z_samples = qz_mu + qz_sigma + torch.randn(num_samples, *qz_mu.shape)
+        z_samples = qz.rsample((num_samples,))
 
         log_px_z = 0
-        kl_py_z_py = 0
-        kl_qz_x_pz_y = 0
+        kl_y = 0
+        kl_z = 0
         for z in z_samples:
             log_px_z += self.loglikelihood(z, x).sum()
 
             py_z = self.py_z(z, pi)
-            kl_py_z_py += py_z * (py_z / pi).log()
+            kl_y += kl_divergence(py_z, Categorical(pi)).sum()
+            # kl_y += py_z * (py_z / pi).log()
 
-            for k in range(pi.shape[-1]):
-                kl_qz_x_pz_y += py_z[:, k] * gaussian_diagonal_kl(
-                    qz_mu, qz_sigma ** 2, self.pz_y_mu[k, :],
-                    self.pz_y_logsigma[k, :].exp() ** 2)
+            # (k, z_dim)
+            pz_y = Normal(self.pz_y_mu, self.pz_y_logsigma.exp())
+            kl_z += (py_z.probs * kl_divergence(qz, pz_y).sum(1)).sum()
+
+            # for k in range(self.k):
+            #     pz_y = Normal(
+            #         self.pz_y_mu[k, :], self.pz_y_logsigma[k, :].exp())
+            #     kl_z_k = py_z.probs[:, k] * kl_divergence(qz, pz_y).sum(1)
+            #     kl_z += kl_z_k.sum()
 
         log_px_z /= num_samples
-        kl_py_z_py /= num_samples
-        kl_qz_x_pz_y /= num_samples
-        elbo = (log_px_z - kl_py_z_py - kl_qz_x_pz_y) / x.shape[0]
+        kl_y /= num_samples
+        kl_z /= num_samples
+        elbo = (log_px_z - kl_y - kl_z) / x.shape[0]
 
         return elbo
+
+    def sample(self, pi=None, num_samples=1):
+        if pi is None:
+            pi = torch.ones(self.k) / self.k
+
+        # Sample p(y).
+        py = Categorical(pi)
+        y = py.sample((num_samples,))
+
+        # Sample p(z|y).
+        pz_y_mu = self.pz_y_mu[y, :]
+        pz_y_sigma = self.pz_y_logsigma[y, :].exp()
+        pz_y = Normal(pz_y_mu, pz_y_sigma)
+        z = pz_y.sample()
+
+        # Sample p(x|z).
+        samples = self.loglikelihood.predict(z)
+
+        return samples
+
+    def predict_x(self, z):
+        x = self.loglikelihood.predict(z)
+
+        return x
+
+    def reconstruct_x(self, x):
+        z, _ = self.encoder(x)
+        x_recon = self.loglikelihood.predict(z)
+
+        return x_recon
